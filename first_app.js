64750acaaba28c00bd229e83fbd179dbbc5f1f3d
Chris Brooker <chris.brooker@live.com>
2016-03-27 00:41:10 +1100

    Commit

--- first_app.js
+++ first_app.js
@@ -4,7 +4,7 @@ if (Meteor.isClient) {
     tasks: [
       { text: "This is task 1" },
       { text: "This is task 2" },
-      { text: "This is task 3" }
+      { text: "This is task 2 + 1" }
     ]
   });
 }
