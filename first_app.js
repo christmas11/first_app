9dd361ad07e9fc070a5dc2394fe61c61a61df711
Chris Brooker <chris.brooker@live.com>
2016-03-27 18:53:42 +1100

    Commit

--- first_app.js
+++ first_app.js
@@ -4,7 +4,21 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+          },
+
+          incompleteCount: function () {
+
+            return Tasks.find({checked: {$ne: true}}).count();
     }
   });
   Template.body.events({
@@ -20,6 +34,9 @@ if (Meteor.isClient) {
       });
       // Clear form
       event.target.text.value = "";
+      },
+      "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
    Template.task.events({
